0f51be0ca438b9281c8ed534cc19f9492e7bef6b
Fredrick Oluoch <menty44@gmail.com>
2018-10-18 17:19:36 +0300

    added ResourceNotFoundException class

---
+++ src/main/java/com/sancom/expo/exception/ResourceNotFoundException.java
@@ -0,0 +1,45 @@
+package com.sancom.expo.exception;
+
+
+/**
+ * Created by admin on 10/18/18.
+ */
+
+/**
+ * Fredrick Oluoch
+ * http://www.blaqueyard.com
+ * 0720106420 | 0722508906
+ * email: menty44@gmail.com
+ */
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public ResourceNotFoundException(String s) {
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+}
