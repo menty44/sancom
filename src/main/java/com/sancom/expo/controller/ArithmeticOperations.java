a2c3891c0457468b276b091563fda3b610cec55a
Fredrick Oluoch <menty44@gmail.com>
2018-10-18 17:15:54 +0300

    added jacoco plugin

---
+++ src/main/java/com/sancom/expo/controller/ArithmeticOperations.java
@@ -0,0 +1,21 @@
+package com.sancom.expo.controller;
+
+
+/**
+ * Created by admin on 10/18/18.
+ */
+
+/**
+ * Fredrick Oluoch
+ * http://www.blaqueyard.com
+ * 0720106420 | 0722508906
+ * email: menty44@gmail.com
+ */
+
+public class ArithmeticOperations {
+
+    public Integer add(Integer a, Integer b)
+    {
+        return a+b;
+    }
+}
--- src/test/java/com/sancom/expo/ExpoApplicationTests.java
+++ src/test/java/com/sancom/expo/ExpoApplicationTests.java
@@ -1,16 +1,27 @@
 package com.sancom.expo;
 
+import com.sancom.expo.controller.ArithmeticOperations;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static junit.framework.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ExpoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//	@Test
+//	public void contextLoads() {
+//	}
 
+	@Test
+	public void testAdd()
+	{
+		ArithmeticOperations operations = new ArithmeticOperations();
+		Integer actual = operations.add(2, 6);
+		Integer expected = 8;
+		assertEquals(expected, actual);
+	}
 }
