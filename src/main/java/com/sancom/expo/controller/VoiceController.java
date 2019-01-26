d6f1e2369936b482d357427237bf55a3db018047
Fredrick Oluoch <menty44@gmail.com>
2019-01-26 07:25:13 +0300

    added voice model and controller

---
+++ src/main/java/com/sancom/expo/controller/VoiceController.java
@@ -0,0 +1,71 @@
+package com.sancom.expo.controller;
+
+/**
+ * Created by admin on 10/18/18.
+ */
+
+import com.sancom.expo.repository.JobseekerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * Fredrick Oluoch
+ * http://www.blaqueyard.com
+ * 0720106420 | 0722508906
+ * email: menty44@gmail.com
+ */
+
+@RestController
+@RequestMapping("/api")
+public class VoiceController {
+
+    @Autowired
+    JobseekerRepository jobseekerRepository;
+
+    private UUID encry = UUID.randomUUID();
+
+    //new user reg
+    @CrossOrigin
+    @RequestMapping(value = "voice", method = RequestMethod.POST, produces = "text/plain")
+    public String registerNewUser(
+            @RequestParam(value = "sessionId") String sessionId,
+            @RequestParam(value = "serviceCode") String serviceCode,
+            @RequestParam(value = "phoneNumber") String phoneNumber,
+            @RequestParam(value = "text") String text
+           ) {
+
+        System.out.println("sessionId : " + sessionId);
+        System.out.println("serviceCode : " + serviceCode);
+        System.out.println("phoneNumber : " + phoneNumber);
+        System.out.println("text: " + text);
+        String[] inputText = text.split("\\*");
+
+        String response = "END The request could not be processed.";
+
+        if (text == null || text.isEmpty()){
+            // This is the first request. Note how we start the responseponse with CON
+            response = "CON What would you want to check \n";
+            response = response + "1. Account Enquiry \n";
+            response = response + "2. Collection \n";
+            response = response + "3. Transfer Fund \n";
+            response = response + "4. Exit \n";
+            System.out.println("response : " + response);
+            return response;
+
+        }else if (text.equals("1")){
+            response = "CON Enter account number \n";
+            return response;
+        }else if (text.equals("2")){
+            response = "CON Collection success \n";
+            return response;
+        }else if (text.equals("3")){
+            response = "END Transfer success \n";
+            return response;
+        }
+
+        return response;
+    }
+
+}
---
+++ src/main/java/com/sancom/expo/model/Voice.java
@@ -0,0 +1,90 @@
+package com.sancom.expo.model;
+
+
+/**
+ * Created by admin on 10/18/18.
+ */
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+//import org.springframework.data.annotation.Id;
+
+/**
+ * Fredrick Oluoch
+ * http://www.blaqueyard.com
+ * 0720106420 | 0722508906
+ * email: menty44@gmail.com
+ */
+
+@Entity
+@Table(name = "ussd")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"},
+        allowGetters = true)
+public class Voice {
+
+//    @Id
+//    private UUID id;
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+
+    private String sessionId;
+
+    private String serviceCode;
+
+    private String phoneNumber;
+
+    private String text;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String getServiceCode() {
+        return serviceCode;
+    }
+
+    public void setServiceCode(String serviceCode) {
+        this.serviceCode = serviceCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
